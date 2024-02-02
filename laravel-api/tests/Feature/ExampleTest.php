973d806f39f39efd1126c46d8d262e260bbc77ca
Njoroge Mwangi <ufalmeart@gmail.com>
2024-02-02 16:14:33 +0300

    Implemented testing for the Laravel API

--- laravel-api/tests/Feature/ExampleTest.php
+++ laravel-api/tests/Feature/ExampleTest.php
@@ -5,14 +5,33 @@ namespace Tests\Feature;
 // use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
-class ExampleTest extends TestCase
+class ApiTest extends TestCase
 {
     /**
      * A basic test example.
      */
-    public function test_the_application_returns_a_successful_response(): void
+    public function test_making_a_post_api_request(): void
     {
-        $response = $this->get('/');
+        $response = $this->postJson(
+            '/api/v1/authors',
+            [
+                "name" => "kevin mwangi",
+                "gender" => "male",
+                "age" => 37,
+                "country" => "Kenya",
+                "genre" => "Romance novel"
+            ]
+        );
+
+        $response
+            ->assertStatus(201);
+    }
+
+    public function test_making_a_get_api_request(): void
+    {
+        $response = $this->get(
+            '/api/v1/books'
+        );
 
         $response->assertStatus(200);
     }
