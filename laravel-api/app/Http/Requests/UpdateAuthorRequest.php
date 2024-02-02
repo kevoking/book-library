5d0b464c741ca29b0711e055f9b9af502ccfe79d
Njoroge Mwangi <ufalmeart@gmail.com>
2024-02-02 10:35:31 +0300

    created form requests to handle data validation

---
+++ laravel-api/app/Http/Requests/UpdateAuthorRequest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Requests;
+
+class UpdateAuthorRequest extends StoreAuthorRequest
+{
+    /**
+     * extend and use the same validation from StoreAuthorRequest
+     */
+    
+}
---
+++ laravel-api/app/Http/Requests/UpdateBookRequest.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Requests;
+
+class UpdateBookRequest extends StoreBookRequest
+{
+    /**
+     * exend an use the same validation in StoreBookRequest
+     */
+}
